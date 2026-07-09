d228653c3dc6c72eb93badea7acdaca4cb1fe434
nabaelhaq-gif <nabaelhaq@gmail.com>
2026-07-09 15:42:20 +0700

    3

---
+++ IntroductionPemdas_0188.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Panjang;
+    string nama = "Abdul";
+    float harga = 50000;
+    double harga2 = 100000;
+
+    cout << "Nama saya : " << nama << endl;
+    cout << "Harga makanan itu : " << harga2;
+}
